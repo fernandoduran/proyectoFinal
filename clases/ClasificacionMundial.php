8c37b2a71bf24240732e327b18cb41b335f8e604
fernandoduran <fduranruiz@gmail.com>
2017-05-28 16:57:11 +0200

    Clase ClasificacionMundial añadida

---
+++ clases/ClasificacionMundial.php
@@ -0,0 +1,535 @@
+<?php
+	
+	/**
+	* 
+	*/
+	class ClasificacionMundial
+	{
+		
+		private $temporada_any;
+		private $pilot_id;
+		private $AU;
+		private $CH;
+		private	$BA;
+		private	$RU;
+		private	$ES;
+		private	$MO;	
+		private $CA;
+		private	$AZ;
+		private	$AT;
+		private	$GR;
+		private $HU;
+		private	$BE;
+		private	$IT;
+		private	$SG;
+		private	$MA;
+		private	$JA;
+		private	$USA;
+		private	$ME;
+		private	$BR;	
+		private $AB;
+
+
+	
+    
+
+    /**
+     * Gets the value of temporada_any.
+     *
+     * @return mixed
+     */
+    public function getTemporadaAny()
+    {
+        return $this->temporada_any;
+    }
+
+    /**
+     * Sets the value of temporada_any.
+     *
+     * @param mixed $temporada_any the temporada any
+     *
+     */
+    public function _setTemporadaAny($temporada_any)
+    {
+        $this->temporada_any = $temporada_any;
+    }
+
+    /**
+     * Gets the value of pilot_id.
+     *
+     * @return mixed
+     */
+    public function getPilotId()
+    {
+        return $this->pilot_id;
+    }
+
+    /**
+     * Sets the value of pilot_id.
+     *
+     * @param mixed $pilot_id the pilot id
+     *
+     * @return self
+     */
+    public function _setPilotId($pilot_id)
+    {
+        $this->pilot_id = $pilot_id;
+    }
+
+    /**
+     * Gets the value of AU.
+     *
+     * @return mixed
+     */
+    public function getAU()
+    {
+        return $this->AU;
+    }
+
+    /**
+     * Sets the value of AU.
+     *
+     * @param mixed $AU the 
+     *
+     * @return self
+     */
+    public function _setAU($AU)
+    {
+        $this->AU = $AU;
+    }
+
+    /**
+     * Gets the value of CH.
+     *
+     * @return mixed
+     */
+    public function getCH()
+    {
+        return $this->CH;
+    }
+
+    /**
+     * Sets the value of CH.
+     *
+     * @param mixed $CH the 
+     *
+     * @return self
+     */
+    public function _setCH($CH)
+    {
+        $this->CH = $CH;
+    }
+
+    /**
+     * Gets the value of BA.
+     *
+     * @return mixed
+     */
+    public function getBA()
+    {
+        return $this->BA;
+    }
+
+    /**
+     * Sets the value of BA.
+     *
+     * @param mixed $BA the 
+     *
+     * @return self
+     */
+    public function _setBA($BA)
+    {
+        $this->BA = $BA;
+    }
+
+    /**
+     * Gets the value of RU.
+     *
+     * @return mixed
+     */
+    public function getRU()
+    {
+        return $this->RU;
+    }
+
+    /**
+     * Sets the value of RU.
+     *
+     * @param mixed $RU the 
+     *
+     * @return self
+     */
+    public function _setRU($RU)
+    {
+        $this->RU = $RU;
+    }
+
+    /**
+     * Gets the value of ES.
+     *
+     * @return mixed
+     */
+    public function getES()
+    {
+        return $this->ES;
+    }
+
+    /**
+     * Sets the value of ES.
+     *
+     * @param mixed $ES the 
+     *
+     */
+    public function _setES($ES)
+    {
+        $this->ES = $ES;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of MO.
+     *
+     * @return mixed
+     */
+    public function getMO()
+    {
+        return $this->MO;
+    }
+
+    /**
+     * Sets the value of MO.
+     *
+     * @param mixed $MO the 
+     *
+     */
+    public function _setMO($MO)
+    {
+        $this->MO = $MO;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of CA.
+     *
+     * @return mixed
+     */
+    public function getCA()
+    {
+        return $this->CA;
+    }
+
+    /**
+     * Sets the value of CA.
+     *
+     * @param mixed $CA the 
+     *
+     */
+    public function _setCA($CA)
+    {
+        $this->CA = $CA;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of AZ.
+     *
+     * @return mixed
+     */
+    public function getAZ()
+    {
+        return $this->AZ;
+    }
+
+    /**
+     * Sets the value of AZ.
+     *
+     * @param mixed $AZ the 
+     *
+     */
+    public function _setAZ($AZ)
+    {
+        $this->AZ = $AZ;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of AT.
+     *
+     * @return mixed
+     */
+    public function getAT()
+    {
+        return $this->AT;
+    }
+
+    /**
+     * Sets the value of AT.
+     *
+     * @param mixed $AT the 
+     *
+     */
+    public function _setAT($AT)
+    {
+        $this->AT = $AT;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of GR.
+     *
+     * @return mixed
+     */
+    public function getGR()
+    {
+        return $this->GR;
+    }
+
+    /**
+     * Sets the value of GR.
+     *
+     * @param mixed $GR the 
+     *
+     */
+    public function _setGR($GR)
+    {
+        $this->GR = $GR;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of HU.
+     *
+     * @return mixed
+     */
+    public function getHU()
+    {
+        return $this->HU;
+    }
+
+    /**
+     * Sets the value of HU.
+     *
+     * @param mixed $HU the 
+     *
+     */
+    public function _setHU($HU)
+    {
+        $this->HU = $HU;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of BE.
+     *
+     * @return mixed
+     */
+    public function getBE()
+    {
+        return $this->BE;
+    }
+
+    /**
+     * Sets the value of BE.
+     *
+     * @param mixed $BE the 
+     *
+     */
+    public function _setBE($BE)
+    {
+        $this->BE = $BE;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of IT.
+     *
+     * @return mixed
+     */
+    public function getIT()
+    {
+        return $this->IT;
+    }
+
+    /**
+     * Sets the value of IT.
+     *
+     * @param mixed $IT the 
+     *
+     */
+    public function _setIT($IT)
+    {
+        $this->IT = $IT;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of SG.
+     *
+     * @return mixed
+     */
+    public function getSG()
+    {
+        return $this->SG;
+    }
+
+    /**
+     * Sets the value of SG.
+     *
+     * @param mixed $SG the 
+     *
+     */
+    public function _setSG($SG)
+    {
+        $this->SG = $SG;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of MA.
+     *
+     * @return mixed
+     */
+    public function getMA()
+    {
+        return $this->MA;
+    }
+
+    /**
+     * Sets the value of MA.
+     *
+     * @param mixed $MA the 
+     *
+     */
+    public function _setMA($MA)
+    {
+        $this->MA = $MA;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of JA.
+     *
+     * @return mixed
+     */
+    public function getJA()
+    {
+        return $this->JA;
+    }
+
+    /**
+     * Sets the value of JA.
+     *
+     * @param mixed $JA the 
+     *
+     */
+    public function _setJA($JA)
+    {
+        $this->JA = $JA;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of USA.
+     *
+     * @return mixed
+     */
+    public function getUSA()
+    {
+        return $this->USA;
+    }
+
+    /**
+     * Sets the value of USA.
+     *
+     * @param mixed $USA the 
+     *
+     */
+    public function _setUSA($USA)
+    {
+        $this->USA = $USA;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of ME.
+     *
+     * @return mixed
+     */
+    public function getME()
+    {
+        return $this->ME;
+    }
+
+    /**
+     * Sets the value of ME.
+     *
+     * @param mixed $ME the 
+     *
+     */
+    public function _setME($ME)
+    {
+        $this->ME = $ME;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of BR.
+     *
+     * @return mixed
+     */
+    public function getBR()
+    {
+        return $this->BR;
+    }
+
+    /**
+     * Sets the value of BR.
+     *
+     * @param mixed $BR the 
+     *
+     */
+    public function _setBR($BR)
+    {
+        $this->BR = $BR;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of AB.
+     *
+     * @return mixed
+     */
+    public function getAB()
+    {
+        return $this->AB;
+    }
+
+
+
+    /**
+     * Sets the value of AB.
+     *
+     * @param mixed $AB the 
+     *
+     */
+    public function _setAB($AB)
+    {
+        $this->AB = $AB;
+    }
+}
+?>
