c7793ffd1e70ef9efbd7555ccd91a320621d993c
fernandoduran <fduranruiz@gmail.com>
2017-05-28 16:56:33 +0200

    Carpeta con archivos para los circuitos
    Incluye index.php, index2.php y contingut.php

---
+++ circuitos/contingut.php
@@ -0,0 +1,16 @@
+<?php
+	
+	if(isset()){
+
+	} else {
+
+		switch ($_GET['sec']) {
+			case 'lista_circuitos':
+				include 'lista_circuitos.php';
+				break;
+			case 'pais':
+				include 'buscador_circuitos.php';
+				break;
+		}
+	}
+?>
---
+++ circuitos/index2.php
@@ -0,0 +1,3 @@
+<?php
+	include("../include/estructura2.php");
+?>
