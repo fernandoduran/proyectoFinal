8989a64f8cde666bfc78770c57151d5d975841f9
fernandoduran <fduranruiz@gmail.com>
2017-06-03 14:36:13 +0200

    Linkeo a script de jquery
    Eliminado por incompatibilidad de versiones

--- include/estructura.php
+++ include/estructura.php
@@ -41,8 +41,8 @@
 
 	<!-- DataTables -->
 	<link rel="stylesheet" type="text/css" href="../DataTables/datatables.min.css"/>
-	 
 	<script type="text/javascript" src="../DataTables/datatables.min.js"></script>
+
 </head>
 <!-- Llibreries -->
 <?php
