3e11864d9803d4337b7e2679729e07206293b032
fernandoduran <fduranruiz@gmail.com>
2017-06-05 00:10:31 +0200

    Archivo para mostrar las fichas de los pilotos

---
+++ pilotos/ficha_piloto.php
@@ -0,0 +1,8 @@
+<?echo titular('Ficha del piloto')?>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-sm-12 col-xs-12">
+			<?echo fichaPiloto($connect, $_GET['id'])?>
+		</div>
+	</div>
+</div>
